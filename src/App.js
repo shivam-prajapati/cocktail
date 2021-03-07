2a196bf4c78bd8d7f36f762460697beb7e58b13f
shivam <shivamprajapatigola@gmail.com>
2021-03-07 12:24:42 +0530

    hello
    Signed-off-by: shivam <shivamprajapatigola@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Seperator from '../src/components/Seperator'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Seperator />
+      <nav class="top_nav"><p class="logo_cocktail">COCKTAIL</p></nav>
+      <Seperator />
     </div>
   );
 }
